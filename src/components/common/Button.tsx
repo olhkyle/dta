2cfe891a352db1407aac4f46a3f2fa86bff87c88
olhkyle <kylekwon96@gmail.com>
2023-09-06 19:20:34 +0900

    ✨ [FEAT] add common components Button, Input, Text

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,18 +1,25 @@
-import { ReactNode } from 'react';
-import styled from '@emotion/styled';
+import { HTMLAttributes, ReactNode } from 'react';
 
-const SButton = styled.button`
-	padding: 0.6rem 1.2rem;
-	border-radius: 8px;
-	font-size: 1rem;
-	font-weight: 600;
-`;
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+	type: 'button' | 'submit';
+	onClick?: () => void;
+	children?: ReactNode;
+}
 
-const Button = ({ children, onClick, ...props }: { children: ReactNode; onClick: () => void }) => {
+const Button = ({ type, onClick, children, ...props }: ButtonProps) => {
 	return (
-		<SButton type="button" onClick={onClick} {...props}>
+		<button
+			type={type}
+			css={{
+				padding: '0.8rem 1.2rem',
+				borderRadius: '8px',
+				fontSize: '16px',
+				fontWeight: '600',
+			}}
+			onClick={onClick}
+			{...props}>
 			{children}
-		</SButton>
+		</button>
 	);
 };
 
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,80 @@
-import React from 'react';
+import { Children, ForwardedRef, HTMLAttributes, ReactElement, ReactNode, cloneElement, forwardRef } from 'react';
+import styled from '@emotion/styled';
+import { useId } from '../../hooks/useId';
 
-const Input = () => {
-	return <>Input</>;
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+	label?: ReactNode;
+	children: ReactElement;
+	bottomText?: string;
+}
+
+const Input = ({ label, children, bottomText, ...props }: InputProps) => {
+	const child = Children.only(children);
+	const generatedId = useId('input');
+	const id = child.props.id ?? generatedId;
+	const isError: boolean = child.props.error ?? false;
+
+	return (
+		<div css={{ display: 'flex', flexDirection: 'column' }} {...props}>
+			<label
+				htmlFor={id}
+				css={{
+					display: `${label ? 'inline-block' : 'none'}`,
+					padding: '4px 0',
+					fontSize: '16px',
+					fontWeight: '500',
+					lineHeight: 1.6,
+					color: 'var(--text-color)',
+				}}>
+				{label}
+			</label>
+			{cloneElement(child, { id, ...child.props })}
+			{bottomText !== null ? <BottomText isError={isError}>{bottomText}</BottomText> : null}
+		</div>
+	);
 };
+
+interface TextFieldProps extends Omit<HTMLAttributes<HTMLInputElement>, 'size'> {
+	type: 'text' | 'password';
+	name: string;
+	placeholder: string;
+	error: string;
+	width: number;
+}
+
+Input.TextField = forwardRef(({ type, name, placeholder, error, width, ...props }: TextFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
+	return (
+		<TextField type={type} name={name} placeholder={placeholder} ref={ref} error={error} autoComplete="off" width={width} {...props} />
+	);
+});
+
+const BottomText = styled.p<{ isError: boolean }>`
+	display: inline-block;
+	margin-top: 4px;
+	color: ${({ isError }) => (isError ? 'var(--color-green-50)' : 'var(--color-gray-400)')};
+	font-size: 14px;
+	font-weight: 400;
+`;
+
+const TextField = styled.input<{ width: number; error: string }>`
+	margin: 0;
+	padding: 0.75rem 1rem;
+	width: 340px;
+	font-size: 14px;
+	line-height: 24px;
+	border: none;
+	border-radius: 8px;
+	color: var(--text-color);
+	outline: none;
+	box-shadow: ${({ error }) => (error ? 'inset 0 0 0 1px var(--color-green-50)' : 'inset 0 0 0 1px var(--outline-color)')};
+
+	&:focus {
+		box-shadow: ${({ error }) => (error ? 'inset 0 0 0 2px var(--color-green-50)' : 'inset 0 0 0 1px var(--text-color)')};
+	}
+
+	@media screen and (min-width: 640px) {
+		width: ${({ width }) => `${width}px`};
+	}
+`;
 
 export default Input;
