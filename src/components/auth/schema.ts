6f91e2b75f741624817c5cf227b232c58ea29e4c
olhkyle <kylekwon96@gmail.com>
2023-09-08 02:55:09 +0900

    [REFAC] move Type export into SignInSchema from SignInForm component

--- src/components/auth/schema.ts
+++ src/components/auth/schema.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
-const signInSchema = z.object({
+export type SigninSchema = z.infer<typeof signInSchema>;
+
+export const signInSchema = z.object({
 	email: z.string().nonempty('이메일을 입력해 주세요').email({ message: '이메일을 입력해 주세요' }),
 	password: z
 		.string()
@@ -9,5 +11,3 @@ const signInSchema = z.object({
 			message: `숫자, 소문자를 포함한 최소 8자, 최대 15자의 비밀번호를 입력해 주세요.`,
 		}),
 });
-
-export { signInSchema };
