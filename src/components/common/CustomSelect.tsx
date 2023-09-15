104fcfc864e5ae6ccc694ae3c203d2c28353ec39
olhkyle <kylekwon96@gmail.com>
2023-09-16 01:03:14 +0900

    [FEAT] update CustomSelect Props and Style

--- src/components/common/CustomSelect.tsx
+++ src/components/common/CustomSelect.tsx
@@ -1,24 +1,26 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { useClickOutside, useId } from '../../hooks';
 import styled from '@emotion/styled';
 import { BiSolidDownArrow } from 'react-icons/bi';
 import { BsCheck } from 'react-icons/bs';
 
 interface SelectProps {
-	data: readonly string[];
-	defaultValue: string;
+	data: readonly number[];
+	value: number;
+	setValue: Dispatch<SetStateAction<number>>;
+	unit?: string;
 	width: number;
 }
 
-const Select = ({ data, defaultValue, width }: SelectProps) => {
+const Select = ({ data, value: current, setValue, unit, width }: SelectProps) => {
 	const [open, setOpen] = useState(false);
-	const [current, setCurrent] = useState(defaultValue);
+
 	const generatedId = useId('custom-select');
 	const generatedListId = useId('custom-select-list');
 
 	const ref = useClickOutside(() => setOpen(false));
 
-	const isCurrent = (value: string) => (current === value ? true : false);
+	const isCurrent = (value: number) => (current === value ? true : false);
 
 	return (
 		<Container tabIndex={0} width={width} ref={ref}>
@@ -30,16 +32,18 @@ const Select = ({ data, defaultValue, width }: SelectProps) => {
 				aria-expanded={open}
 				tabIndex={0}
 				width={width}>
-				<span>{current}</span>
+				<span>
+					{current} {unit}
+				</span>
 				<BiSolidDownArrow size="14" />
 			</Trigger>
 			{open && (
-				<Options id={generatedListId} role="listbox" aria-labelledby={generatedId} width={width}>
+				<Options id={generatedListId} role="listbox" aria-labelledby={generatedId}>
 					{data.map(item => (
 						<Option
 							key={item}
 							onClick={() => {
-								setCurrent(item);
+								setValue(item);
 								setOpen(false);
 							}}
 							isCurrent={isCurrent(item)}
@@ -47,6 +51,7 @@ const Select = ({ data, defaultValue, width }: SelectProps) => {
 							tabIndex={0}>
 							<span>{isCurrent(item) && <BsCheck size="20" />}</span>
 							{item}
+							{unit}
 						</Option>
 					))}
 				</Options>
@@ -57,17 +62,15 @@ const Select = ({ data, defaultValue, width }: SelectProps) => {
 
 const Container = styled.div<{ width: number }>`
 	position: relative;
-	width: ${({ width }) => `${width}px`};
 `;
 
 const Trigger = styled.button<{ width: number }>`
 	display: inline-flex;
 	justify-content: center;
 	align-items: center;
-	gap: 0.4rem;
-	padding: 0.75rem 1.4rem;
-	width: ${({ width }) => `${width}px`};
-	font-size: 19px;
+	gap: 0.2rem;
+	padding: 0.6rem 0.75rem;
+	font-size: 15px;
 	font-weight: 600;
 	line-height: 1.2;
 	border: 1px solid var(--outline-color);
@@ -78,9 +81,19 @@ const Trigger = styled.button<{ width: number }>`
 		outline: 2px solid var(--text-color);
 		outline-offset: 2px;
 	}
+
+	@media screen and (min-width: 640px) {
+		gap: 0.4rem;
+		padding: 0.75rem 1.4rem;
+		font-size: 17px;
+	}
+
+	@media screen and (min-width: 720px) {
+		font-size: 19px;
+	}
 `;
 
-const Options = styled.ul<{ width: number }>`
+const Options = styled.ul`
 	position: absolute;
 	top: 110%;
 	border: 1px solid var(--outline-color);
@@ -95,8 +108,8 @@ const Option = styled.li<{ isCurrent: boolean }>`
 	align-items: center;
 	gap: 0.2rem;
 	width: 100%;
-	padding: 0.5rem 0.6rem 0.5rem 0.1rem;
-	font-size: 17px;
+	padding: 0.5rem 0.4rem 0.5rem 0.1rem;
+	font-size: 15px;
 	font-weight: ${({ isCurrent }) => (isCurrent ? 700 : 500)};
 	color: var(--text-color);
 	cursor: pointer;
@@ -104,6 +117,15 @@ const Option = styled.li<{ isCurrent: boolean }>`
 	&:hover {
 		background-color: var(--color-green-50);
 		color: var(--color-white);
+	}
+
+	@media screen and (min-width: 640px) {
+		padding: 0.5rem 1rem 0.5rem 0;
+		font-size: 17px;
+	}
+
+	@media screen and (min-width: 720px) {
+		font-size: 19px;
 	}
 
 	span {
