243b03e2fd1b369982548aeb4bc487ea446047fd
olhkyle <kylekwon96@gmail.com>
2023-09-20 03:06:12 +0900

    [STYLE] update NotFound Style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Button, Flex, Text } from '../components';
+import { Button, Circle, Flex, Text } from '../components';
 import { useGoBack } from '../hooks';
 
 const NotFound = () => {
@@ -8,16 +8,27 @@ const NotFound = () => {
 	return (
 		<>
 			<Flex direction="column" margin="10rem auto">
-				<Text typo="h3" color="var(--text-color)">
+				<Text typo="h4" color="var(--text-color)">
 					서비스에 문제가 발생하였습니다 🚀
 				</Text>
-				<Flex direction="column" margin="2rem">
+				<CustomFlex direction="column" margin="2rem">
 					<Text color="var(--text-color)">방문하시려는 페이지의 주소가 잘못 입력되었거나, 삭제되어 사용하실 수 없습니다.</Text>
 					<Text color="var(--text-color)">입력하신 주소가 정확한지 다시 한 번 확인해 주세요</Text>
-				</Flex>
+				</CustomFlex>
 				<GoBackButton type="button" onClick={goBack}>
 					돌아가기
 				</GoBackButton>
+				<CustomFlex margin="3rem 0" gap="1rem">
+					<Circle size={40} bgColor="var(--color-gray-100)" />
+					<Circle size={40} bgColor="var(--color-gray-200)" />
+					<Circle size={40} bgColor="var(--color-gray-300)" />
+					<Circle size={40} bgColor="var(--color-gray-400)" />
+					<Circle size={40} bgColor="var(--color-gray-500)" />
+					<Circle size={40} bgColor="var(--color-gray-600)" />
+					<Circle size={40} bgColor="var(--color-gray-700)" />
+					<Circle size={40} bgColor="var(--color-gray-800)" />
+					<Circle size={40} bgColor="var(--color-gray-900)" />
+				</CustomFlex>
 			</Flex>
 		</>
 	);
@@ -26,6 +37,15 @@ const NotFound = () => {
 const GoBackButton = styled(Button)`
 	color: var(--bg-color);
 	background-color: var(--color-green-50);
+`;
+
+const CustomFlex = styled(Flex)`
+	flex-direction: column;
+	text-align: center;
+
+	@media screen and (min-width: 640px) {
+		flex-direction: row;
+	}
 `;
 
 export default NotFound;
