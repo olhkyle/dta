3561c5e13ace5b48adab7b0d99b150a62ec9d558
olhkyle <kylekwon96@gmail.com>
2023-09-24 02:17:40 +0900

    [TYPE] add Type on Today variable

--- src/constants/day.ts
+++ src/constants/day.ts
@@ -2,7 +2,7 @@ const months: number[] = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const years: number[] = [2023, 2022, 2021, 2020];
 
-const today = new Date();
+const today: Date = new Date();
 
 const yearOfToday = today.getFullYear();
 
