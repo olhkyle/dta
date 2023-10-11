f8a0310678b205da7d3373b7a5c504d26d495c04
olhkyle <kylekwon96@gmail.com>
2023-10-11 22:30:29 +0900

    [FIX] fix auth/invalid-api-key problem

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,9 +1,17 @@
-import firebaseConfig from './config';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+} as const;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
