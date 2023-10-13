dbb688249f00a4778b2e9b61338debb5bfa13171
olhkyle <kylekwon96@gmail.com>
2023-10-13 18:12:15 +0900

    [FEAT] add fontSize Partial Props

--- src/components/common/HighlightText.tsx
+++ src/components/common/HighlightText.tsx
@@ -3,16 +3,18 @@ import { ReactNode } from 'react';
 interface HighlightTextProps {
 	color: string;
 	bgColor: string;
+	fontSize?: string;
 	children: ReactNode;
 }
 
-const HighlightText = ({ color, bgColor, children }: HighlightTextProps) => {
+const HighlightText = ({ color, bgColor, fontSize = '16px', children }: HighlightTextProps) => {
 	return (
 		<span
 			css={{
 				padding: '0.2rem 0.4rem',
 				backgroundColor: bgColor,
 				color,
+				fontSize,
 				borderRadius: '8px',
 				fontWeight: '600',
 				textAlign: 'center',
