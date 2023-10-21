254e1b3f109cbf2ce1ab82688ed46807b1ae0f2a
olhkyle <kylekwon96@gmail.com>
2023-10-21 22:59:26 +0900

    [REFAC] refactor Loading Component

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -7,64 +7,38 @@ interface LoadingProps {
 	margin?: string;
 }
 
+type LoadingSvgProps = Omit<LoadingProps, 'margin'>;
+
+const LoadingSvg = ({ type = 'lg', size = 70 }: LoadingSvgProps) => (
+	<Svg
+		type={type}
+		size={size}
+		xmlns="http://www.w3.org/2000/svg"
+		xmlnsXlink="http://www.w3.org/1999/xlink"
+		css={{ margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto' }}
+		viewBox="0 0 100 100"
+		preserveAspectRatio="xMidYMid">
+		<circle cx="50" cy="50" fill="none" stroke="#46df8e" strokeWidth="7" r="35" strokeDasharray="164.93361431346415 56.97787143782138">
+			<animateTransform
+				attributeName="transform"
+				type="rotate"
+				repeatCount="indefinite"
+				dur="1s"
+				values="0 50 50;360 50 50"
+				keyTimes="0;1"
+			/>
+		</circle>
+	</Svg>
+);
+
 const Loading = ({ type = 'lg', size = 70, margin = '5rem 0' }: LoadingProps) => {
 	return (
 		<>
 			{type === 'sm' ? (
-				<Svg
-					type={type}
-					size={size}
-					xmlns="http://www.w3.org/2000/svg"
-					xmlnsXlink="http://www.w3.org/1999/xlink"
-					css={{ margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto' }}
-					viewBox="0 0 100 100"
-					preserveAspectRatio="xMidYMid">
-					<circle
-						cx="50"
-						cy="50"
-						fill="none"
-						stroke="#46df8e"
-						strokeWidth="7"
-						r="35"
-						strokeDasharray="164.93361431346415 56.97787143782138">
-						<animateTransform
-							attributeName="transform"
-							type="rotate"
-							repeatCount="indefinite"
-							dur="1s"
-							values="0 50 50;360 50 50"
-							keyTimes="0;1"
-						/>
-					</circle>
-				</Svg>
+				<LoadingSvg type={type} size={size} />
 			) : (
-				<Flex justifyContent="center" margin={margin}>
-					<Svg
-						type={type}
-						size={size}
-						xmlns="http://www.w3.org/2000/svg"
-						xmlnsXlink="http://www.w3.org/1999/xlink"
-						css={{ margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto' }}
-						viewBox="0 0 100 100"
-						preserveAspectRatio="xMidYMid">
-						<circle
-							cx="50"
-							cy="50"
-							fill="none"
-							stroke="#46df8e"
-							strokeWidth="7"
-							r="35"
-							strokeDasharray="164.93361431346415 56.97787143782138">
-							<animateTransform
-								attributeName="transform"
-								type="rotate"
-								repeatCount="indefinite"
-								dur="1s"
-								values="0 50 50;360 50 50"
-								keyTimes="0;1"
-							/>
-						</circle>
-					</Svg>
+				<Flex justifyContent="center" alignItems="center" margin={margin}>
+					<LoadingSvg type={type} size={size} />
 				</Flex>
 			)}
 		</>
