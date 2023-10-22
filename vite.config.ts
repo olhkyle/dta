6b5f733dd87c644298207e8c8e7138db0b49fc01
olhkyle <kylekwon96@gmail.com>
2023-10-22 17:01:52 +0900

    [CHORE] reduce chunk size to build with rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const exclVendors = ['react', 'react-router-dom', 'react-dom'];
+const renderChunks = (deps: Record<string, string>) => {
+	let chunks = {};
+	Object.keys(deps).forEach(key => {
+		if (exclVendors.includes(key)) return;
+		chunks[key] = [key];
+	});
+	return chunks;
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	build: {
 		rollupOptions: {
 			output: {
-				manualChunks(id) {
-					console.log(id);
-					if (id.includes('node_modules')) {
-						return id.toString().split('node_modules/').pop().split('/')[0];
+				manualChunks(id: string) {
+					// Reducing the vendor chunk size
+					if (id.includes('tslib')) {
+						return '@tslib';
+					}
+					// creating a chunk to react routes deps. Reducing the vendor chunk size
+					if (id.includes('react-router-dom') || id.includes('@remix-run') || id.includes('react-router')) {
+						return '@react-router';
+					}
+
+					if (id.includes('firebase')) {
+						return '@firebase';
+					}
+
+					if (id.includes('clsx')) {
+						return '@clsx';
+					}
+
+					if (id.includes('redux-thunk')) {
+						return '@redux-thunk';
 					}
 				},
 			},
