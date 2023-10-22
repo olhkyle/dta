09facb79865b5d139d4763969c9a0c9fa3afb5d7
olhkyle <kylekwon96@gmail.com>
2023-10-22 16:00:28 +0900

    [TEST] test rollback to one bundle index.js for performance optimization

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					console.log(id);
-					if (id.includes('node_modules')) {
-						return id.toString().split('node_modules/').pop().split('/')[0];
-					}
-				},
-			},
-		},
-	},
 });
