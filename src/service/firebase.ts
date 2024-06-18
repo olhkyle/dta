eb3c01b2fb4aa7d58de22c3309376768fd6384fe
olhkyle <kylekwon96@gmail.com>
2024-06-18 11:51:16 +0900

    [FEAT] add firebase app-check function

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,6 +16,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+if (typeof window !== 'undefined') {
+	initializeAppCheck(app, {
+		provider: new ReCaptchaV3Provider(import.meta.env.VITE_FIREBASE_RECAPTCHA_SITE_KEY),
+		isTokenAutoRefreshEnabled: true,
+	});
+}
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
