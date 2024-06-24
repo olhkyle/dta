e6b06ab547d48dc42b257773e1baece03f79abc5
olhkyle <kylekwon.dev@gmail.com>
2024-06-24 17:16:43 +0900

    [FIX] fix missing of current year data on 'months' constant

--- src/constants/day.ts
+++ src/constants/day.ts
@@ -1,11 +1,11 @@
-const months: number[] = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-
-const years: number[] = [2023, 2022, 2021, 2020];
-
 const today: Date = new Date();
 
 const yearOfToday = today.getFullYear();
 
 const monthOfToday = today.getMonth() + 1;
+
+const months: number[] = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const years: number[] = Array.from({ length: 4 }, (_, idx) => (idx === 0 ? yearOfToday : yearOfToday - idx));
 
 export { months, years, today, yearOfToday, monthOfToday };
