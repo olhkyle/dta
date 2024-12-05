8c5070f99e0132aebb6b749e48e1b55ef9afe73e
olhkyle <kylekwon.dev@gmail.com>
2024-12-05 17:14:10 +0900

    fix : 수정 후 details 페이지로 리다이렉트 시, 기존 캐싱 데이터를 보여주는 문제 해결

--- src/loaders/getWorkersDetailLoader.ts
+++ src/loaders/getWorkersDetailLoader.ts
@@ -5,7 +5,7 @@ import getPaginationQuery from '../queries/getWorkersDetailByPageQuery';
 const getWorkersDetailLoader = (queryClient: QueryClient) => async () => {
 	const query = getPaginationQuery(workerQuery);
 
-	return queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchInfiniteQuery(query));
+	return await queryClient.fetchInfiniteQuery(query);
 };
 
 export default getWorkersDetailLoader;
