76d83335cf65eff52472e8e88b12808429d1921b
olhkyle <kylekwon.dev@gmail.com>
2025-04-11 15:46:10 +0900

    style : change font-size of contents in `Content` and `Label`
    - use `useMediaQuery` hook to check if it's mobile or not

--- src/components/dashboard/Content.tsx
+++ src/components/dashboard/Content.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
+import { useMediaQuery } from '../../hooks';
 
 interface ContentProps {
 	children: ReactNode;
 }
 
 const Content = ({ children }: ContentProps) => {
+	const isMobile = useMediaQuery('(max-width: 640px)');
+
 	return (
 		<div
 			css={{
 				display: 'flex',
 				alignItems: 'center',
 				gap: '4px',
-				fontSize: 'var(--fz-h4)',
+				fontSize: isMobile ? 'var(--fz-h4)' : 'var(--fz-h3)',
 				fontWeight: 'var(--fw-bold)',
 			}}>
 			{children}
--- src/components/dashboard/Label.tsx
+++ src/components/dashboard/Label.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
+import { useMediaQuery } from '../../hooks';
 
 interface LabelProps {
 	children: ReactNode;
 }
 
 const Label = ({ children }: LabelProps) => {
+	const isMobile = useMediaQuery('(max-width: 640px)');
+
 	return (
 		<div
 			css={{
@@ -12,7 +15,7 @@ const Label = ({ children }: LabelProps) => {
 				justifyContent: 'space-between',
 				alignItems: 'center',
 				width: '100%',
-				fontSize: 'var(--fz-h7)',
+				fontSize: isMobile ? 'var(--fz-md)' : 'var(--fz-h7)',
 				fontWeight: 'var(--fw-semibold)',
 			}}>
 			{children}
