ab0d5ebf841da7843b735ea738d9743d4b8128e6
olhkyle <kylekwon.dev@gmail.com>
2025-04-12 17:09:59 +0900

    feat : add `DashboardLink` on `UserProfile` and `SideNav`'s Name Button

--- src/components/layout/SideNav.tsx
+++ src/components/layout/SideNav.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import { Button, Flex, NavLink, Text, ThemeButton } from '..';
 import { useClickOutside, useMediaQuery, useSetUser, useSideNavActive } from '../../hooks';
 import { routes } from '../../constants';
+import { Link } from 'react-router-dom';
 
 interface SideNavProps {
 	isShown: boolean;
@@ -62,14 +63,21 @@ const SideNav = ({ isShown, isLoading, Loading, onLogout }: SideNavProps) => {
 							👨‍🚀 {name}
 						</Text>
 						{isProfileClicked && (
-							<LogoutButton
-								type="button"
-								onClick={() => {
-									onLogout();
-									close();
-								}}>
-								{isLoading ? <Loading /> : '로그아웃'}
-							</LogoutButton>
+							<CustomFlex direction={'column'} aria-haspopup="true">
+								{isAdmin && (
+									<DashboardLink to={routes.DASHBOARD} onClick={close}>
+										대시보드
+									</DashboardLink>
+								)}
+								<LogoutButton
+									type="button"
+									onClick={() => {
+										onLogout();
+										close();
+									}}>
+									{isLoading ? <Loading /> : '로그아웃'}
+								</LogoutButton>
+							</CustomFlex>
 						)}
 					</Name>
 				) : (
@@ -142,10 +150,36 @@ const Name = styled.div`
 	cursor: pointer;
 `;
 
-const LogoutButton = styled(Button)`
+const CustomFlex = styled(Flex)`
 	position: absolute;
-	bottom: -50px;
-	padding: calc(var(--padding-md) * 0.75) calc(var(--padding-md) * 1.25);
+	top: 48px;
+	border: 1px solid var(--border-light-color);
+	border-radius: var(--radius);
+`;
+
+const DashboardLink = styled(Link)`
+	padding: calc(var(--padding-md) * 0.8) var(--padding-md);
+	min-width: 120px;
+	width: 100%;
+	line-height: 1;
+	font-size: var(--fz-sm);
+	font-weight: var(--fw-medium);
+	color: var(--text-color);
+	background-color: var(--bg-color);
+	border-radius: var(--radius);
+	text-align: center;
+
+	&:focus {
+		background-color: var(--btn-light-bg-color);
+	}
+
+	@media screen and (min-width: 768px) {
+		font-size: var(--fz-h7);
+	}
+`;
+
+const LogoutButton = styled(Button)`
+	padding: calc(var(--padding-md) * 0.8) var(--padding-md);
 	width: 100%;
 	min-width: 120px;
 	font-size: var(--fz-sm);
@@ -153,13 +187,12 @@ const LogoutButton = styled(Button)`
 	line-height: 1;
 	color: var(--text-color);
 	background-color: var(--bg-color);
-	border-radius: 8px;
-	border: 1px solid var(--border-light-color);
+	border-radius: var(--radius);
 	text-align: center;
 	transition: font-weight 0.15s ease-in-out;
 
-	&:hover {
-		font-weight: var(--fw-semibold);
+	&:focus {
+		background-color: var(--btn-light-bg-color);
 	}
 
 	@media screen and (min-width: 768px) {
